9281f9eb5fa6f5860182f3b7a2ede0ba4db51961
Keesha Hargrove <kharg14@gmail.com>
2020-10-03 11:44:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-<form method="POST" action="/food">
-  <p>Your Name: <input type="text" name="name"></p>
-  <p>Your Favorite Food: <input type="text" name="favorite_food"></p>
-  <input type="submit">
+      <form method="POST" action="/food">
+      <p>Your Name: <input type="text" name="name"></p>
+      <p>Your Favorite Food: <input type="text" name="favorite_food"></p>
+      <input type="submit">
 </form>
 end
